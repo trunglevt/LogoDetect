6462e08a7edbecc9df6d71cffa79306543b67e45
trunglevt <33653610+trunglevt@users.noreply.github.com>
2017-11-30 11:10:11 +0700

    Update test5.py

--- test5.py
+++ test5.py
@@ -1,46 +1,67 @@
+import csv
+from subprocess import Popen
 import subprocess
+import time
 import cv2
-import numpy as np 
+import numpy as np
+import os.path
+import os
 
-web = 'udp://' UDP multicast link here
-subprocess.call(['ffmpeg', '-i', web, '-vf', "select='eq(pict_type\,I)+eq(pict_type\,P)'","-vframes","1","-y",'thump.jpg'])
-from datetime import datetime
-startTime= datetime.now()
+with open('ip.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for line in csv_reader:
+        web = line['Multicast_IP']
+        web2 = 'udp://@{}?pkt_size=1480'.format(web)
+        print(web2)
+        proc = subprocess.Popen(
+            ['ffmpeg', '-i', web2, '-vf', "select='eq(pict_type\,I)+eq(pict_type\,P)'", "-vframes", "1", "-y",
+             'thump.jpg']) #calling subprocess
+        time.sleep(10) #wait 10s here
+        if (os.path.isfile('thump.jpg') == True): #check if it produces a keyframe, if not kill process and move to another IP 
+            MIN_MATCH_COUNT = 5
 
+            img1 = cv2.imread('thump.jpg')  # anh_logo
+            img2 = cv2.imread('result.png')  # anh_muon_xet
 
-MIN_MATCH_COUNT = 5
+            img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+            img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
-img1 = cv2.imread('thump.jpg')# anh_logo
-img2 = cv2.imread('result.png') # anh_muon_xet
+            sift = cv2.xfeatures2d.SIFT_create()
 
-img1= cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
-img2= cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
-# Initiate SIFT detector
-sift = cv2.xfeatures2d.SIFT_create()
+            kp1, des1 = sift.detectAndCompute(img1, None)
+            kp2, des2 = sift.detectAndCompute(img2, None)
 
-# find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1,None)
-kp2, des2 = sift.detectAndCompute(img2,None)
+            FLANN_INDEX_KDTREE = 0
+            index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+            search_params = dict(checks=50)
 
-FLANN_INDEX_KDTREE = 0
-index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-search_params = dict(checks = 50)
+            flann = cv2.FlannBasedMatcher(index_params, search_params)
 
-flann = cv2.FlannBasedMatcher(index_params, search_params)
+            matches = flann.knnMatch(des1, des2, k=2)
 
-matches = flann.knnMatch(des1,des2,k=2)
+            good = []
+            for m, n in matches:
+                if m.distance < 0.7 * n.distance:
+                    good.append(m)
 
-# store all the good matches as per Lowe's ratio test.
-good = []
-for m,n in matches:
-    if m.distance < 0.7*n.distance:
-        good.append(m)
+            if len(good) > MIN_MATCH_COUNT:
+                print("we have {} matches".format(len(good)))
+                print("We found logo FPT")
+                os.remove('thump.jpg')     #right now, file is removed so storage will not be affected
 
-if len(good)>MIN_MATCH_COUNT:
-    print ("we have {} matches".format(len(good)))
-    print("We found logo FPT")
-else:
-    print ("Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT))
-timeElapsed=datetime.now()-startTime
+                with open('ip2.csv', 'a') as newFile: # the result will be put in here
+                    newFileWriter = csv.writer(newFile)
+                    newFileWriter.writerow(['yes'])
 
-print('Time elpased (hh:mm:ss.ms) {}'.format(timeElapsed))
+            else:
+                print("Not enough matches are found - %d/%d" % (len(good), MIN_MATCH_COUNT))
+                os.remove('thump.jpg')
+                with open('ip2.csv', 'a') as newFile:
+                    newFileWriter = csv.writer(newFile)
+                    newFileWriter.writerow(['no'])
+        else:
+            with open('ip2.csv', 'a') as newFile:
+                newFileWriter = csv.writer(newFile)
+                newFileWriter.writerow(['not available'])
+            proc.kill()  #killing process and move on 
+csv_file.close()
